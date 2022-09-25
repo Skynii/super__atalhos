789a798203de646598a0ef67cf3da9d95eb3e5ed
Érica Joice dos Santos <ejoice19@gmail.com>
2022-09-25 16:26:42 -0300

    botam sistemas funcionando

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const sistemaMenu = document.querySelector('#sistemas');
-const pessoasMenu = document.querySelector('#pessoas');
-const resultadoMenu = document.querySelector('#resultado');
-const telefoniaMenu = document.querySelector('#telefonia');
-const vmMenu = document.querySelector('#vm');
+const sistemaMenu = document.getElementById('sistemas');
+const pessoasMenu = document.getElementById('pessoas');
+const resultadoMenu = document.getElementById('resultado');
+const telefoniaMenu = document.getElementById('telefonia');
+const vmMenu = document.getElementById('vm');
+const buttonSection = document.querySelector('.button-section')
 
+//* Adicionar evento de click e testar printando na tela
 
+function click(){
+   sistemaMenu.addEventListener('click', function(){
+      console.log('Clicou em sistemas')
+   })
+}
 
+click();
 //**Adicionar botoes */
 
 //**Um tela para cada botão clicado */
