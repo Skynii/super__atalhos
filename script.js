72106a35fbe8058246a23dadbde1082bbb18f613
Érica Joice dos Santos <ejoice19@gmail.com>
2022-09-25 16:15:54 -0300

    Nav bar criado

---
+++ script.js
@@ -0,0 +1,11 @@
+const sistemaMenu = document.querySelector('#sistemas');
+const pessoasMenu = document.querySelector('#pessoas');
+const resultadoMenu = document.querySelector('#resultado');
+const telefoniaMenu = document.querySelector('#telefonia');
+const vmMenu = document.querySelector('#vm');
+
+
+
+//**Adicionar botoes */
+
+//**Um tela para cada botão clicado */
