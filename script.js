1f462782bcce5dcbdaf8a9f9526e914248a716f3
Érica Joice dos Santos <ejoice19@gmail.com>
2022-09-25 16:38:34 -0300

    Botões funcionando.

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 const sistemaMenu = document.getElementById('sistemas');
 const pessoasMenu = document.getElementById('pessoas');
-const resultadoMenu = document.getElementById('resultado');
+const resultadoMenu = document.getElementById('resultados');
 const telefoniaMenu = document.getElementById('telefonia');
 const vmMenu = document.getElementById('vm');
+const digitalMenu = document.getElementById('digital');
 const buttonSection = document.querySelector('.button-section')
 
-//* Adicionar evento de click e testar printando na tela
+//**Adicionar botoes */
+
+//**Um tela para cada botão clicado */
+
+
+
+
+
+
+
+
+/************************************* */
+//* Adicionar evento de click e testar  na tela
 
 function click(){
    sistemaMenu.addEventListener('click', function(){
       console.log('Clicou em sistemas')
-   })
+   });
+   pessoasMenu.addEventListener('click', function(){
+      console.log('Agora está em Pessoas')
+   });
+   resultadoMenu.addEventListener('click', function(){
+      console.log('Agora está em Resultados');
+   });
+   telefoniaMenu.addEventListener('click', function(){
+      console.log('Agora está em Telefonia');
+   });
+   vmMenu.addEventListener('click', function(){
+      console.log('Agora está em Visual Merchansing');
+   });
+   digitalMenu.addEventListener('click', function(){
+      console.log('Agora está em Visual digital');
+   });
 }
 
 click();
-//**Adicionar botoes */
 
-//**Um tela para cada botão clicado */
