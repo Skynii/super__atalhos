2ed11ce0485d26cac354dac84fa67169269c0d32
Érica Joice dos Santos <ejoice19@gmail.com>
2022-10-02 11:12:24 -0300

    remoção funcionando

--- script.js
+++ script.js
@@ -8,46 +8,94 @@ const vmMenu = document.getElementById('vm');
 const digitalMenu = document.getElementById('digital');
 let buttonSe = document.getElementsByClassName('button');
 
-
-
-let selectionSistemas = document.querySelectorAll('.sistemas');
-let seleResultados = document.querySelectorAll('.resultados');
-let selectionTelefonia = document.querySelector('.telefonia');
-
-
-function sistemas(){
-   sistemaMenu.addEventListener('click', function(){
-      for(let i = 0; i < seleResultados.length; i++) {
-         seleResultados.item(i).style.display ="none"
-      }
-      
-   })
+function sistemas(){ 
+   let selectionSistemas = document.querySelectorAll('.sistemas');  
+   for(let i = 0; i < selectionSistemas.length; i++) {
+      selectionSistemas.item(i).style.display ="none";
+   }
+};
+//Funções que removem a classe
+function pessoas(){
+   let selePessoas = document.querySelectorAll('.pessoas');   
+   for(let i = 0; i < selePessoas.length; i++) {
+      selePessoas.item(i).style.display ="none";
+   }
 };
 
-sistemas();
+function telefonia(){ 
+   let selectionTelefonia = document.querySelectorAll('.telefonia');  
+   for(let i = 0; i < selectionTelefonia.length; i++) {
+      selectionTelefonia.item(i).style.display ="none";
+   }
+};
 
-/*function click(){
+function resultados(){
+   let seleResultados = document.querySelectorAll('.resultados');
+   for(let i = 0; i < seleResultados.length; i++) {
+         seleResultados.item(i).style.display ="none";
+      }
+};
+
+function vm(){
+   let seleVM = document.querySelectorAll('.vm');
+   for(let i = 0; i < seleVM.length; i++) {
+      seleVM.item(i).style.display ="none";
+      }
+};
+function digital(){
+   let seleDigital = document.querySelectorAll('.digital');
+   for(let i = 0; i < seleDigital.length; i++) {
+      seleDigital.item(i).style.display ="none";
+      }
+};
+
+
+//Função que remove ao clicar 
+function click(){
    sistemaMenu.addEventListener('click', function(){
-      
+      resultados();
+      pessoas();
+      telefonia();
+      vm();
+      digital();
    });
    pessoasMenu.addEventListener('click', function(){
-      
+      resultados();
+      sistemas();
+      telefonia();
+      vm();
+      digital();
       
    });
    resultadoMenu.addEventListener('click', function(){
-      console.log('Agora está em Resultados');
+      pessoas();
+      sistemas();
+      telefonia();
+      vm();
+      digital();
    });
    telefoniaMenu.addEventListener('click', function(){
-      console.log('Agora está em Telefonia');
+      pessoas();
+      sistemas();
+      resultados();
+      vm();
+      digital();
    });
    vmMenu.addEventListener('click', function(){
-      console.log('Agora está em Visual Merchansing');
+      pessoas();
+      sistemas();
+      resultados();
+      telefonia();
+      digital();
    });
    digitalMenu.addEventListener('click', function(){
-      console.log('Agora está em Visual digital');
+      pessoas();
+      sistemas();
+      resultados();
+      telefonia();
+      vm();
    });
 }
 
 click();
-*/
 
